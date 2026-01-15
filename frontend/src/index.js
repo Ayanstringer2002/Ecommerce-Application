633e80705b45cc20b856e988874cfb3b32884e73
marcusayan <ayan988ahmad@gmail.com>
2026-01-16 02:20:56 +0530

    index.js updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { SnackbarProvider } from 'notistack';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <SnackbarProvider
+        maxSnack={2}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+      >
+        <Router>
+          <App />
+        </Router>
+      </SnackbarProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+
+Logical Summary of the Code
+
+This file is the entry point of a React application.
+
+It initializes React and renders the app into the browser.
+
+Sets up Redux for global state management.
+
+Enables client-side routing using React Router.
+
+Configures snackbar notifications globally using Notistack.
+
+Wraps everything in StrictMode for better debugging and code quality.
+
+*/
