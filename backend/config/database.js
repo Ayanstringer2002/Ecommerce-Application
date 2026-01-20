9eaf28cc41e47db9678dc9bb9f44b9d39dc0b78f
marcusayan <ayan988ahmad@gmail.com>
2026-01-20 15:33:23 +0530

    added databse.js

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// Read MongoDB connection URI from environment variables
+const MONGO_URI = process.env.MONGO_URI;
+
+// fUNCTION TO CONNECT TO MongoDB
+const connectDatabase = () => {
+    mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})  // useNewUrlParser: Uses the modern MongoDB URL parser
+       .then(() => {                                                                // useUnifiedTopology: Uses the new MongoDB server discovery engine
+          console.log("Mongoose Connected");
+       });
+}
+
+// Export the database connection function
+module.exports = connectDatabase;
